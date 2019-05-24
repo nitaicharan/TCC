e9bd86b0d591910435ea6b1c5820558a203ac7ca
Nitai Charan <nitaicharan@gmail.com>
2019-05-24 10:19:28 -0300

    Update sexta 24 maio 2019, 10:19:28

--- Modelo AbnTex2/git.sh
+++ Modelo AbnTex2/git.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git add --all 1> /tmp/pdflatex1 2> /tmp/pdflatex1
-git commit -a -m "Update `date`" 1> /tmp/pdflatex2 2> /tmp/pdflatex2
+git commit -a -m "Update `date "+%A %d %B %Y, %H:%M:%S"`" 1> /tmp/pdflatex2 2> /tmp/pdflatex2
 git push origin master 1> /tmp/pdflatex3 2> /tmp/pdflatex3
