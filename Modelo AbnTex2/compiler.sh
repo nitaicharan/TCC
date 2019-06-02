70cd1b03fe26f313a21a12b3956b2fed80255e19
Nitai Charan <nitaicharan@gmail.com>
2019-06-02 11:32:35 -0300

    Update domingo 02 junho 2019, 11:32:35

--- Modelo AbnTex2/compiler.sh
+++ Modelo AbnTex2/compiler.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . | grep -v .swp$ | entr /bin/bash -c " reset ;notify-send 'Convertento imagens PNG em EPS' ;mogrify -format eps -- imagens/*.png ;notify-send 'Compilando .tex' ;latex TCC.tex ;notify-send 'Extraindo referências' ;bibtex TCC.aux ;notify-send 'Criando PDF' ; pdflatex  TCC.tex ;notify-send 'Criando PDF' ;pdflatex  TCC.tex ;notify-send 'Push das modificações' ; ./git.sh ;notify-send 'Processo finalizado!' "
+find . | grep -v .swp$ | entr /bin/bash -c " reset ;notify-send 'Convertento imagens PNG em EPS' ;mogrify -format eps -- imagens/*.png ;notify-send 'Compilando .tex' ;latex TCC.tex ;notify-send 'Extraindo referências' ;bibtex TCC.aux ;notify-send 'Criando PDF' ;pdflatex  TCC.tex ;notify-send 'Push das modificações' ; ./git.sh ;notify-send 'Processo finalizado!' "
